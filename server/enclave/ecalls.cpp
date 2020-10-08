c9cf6e1338a7de9335ecbcf4d532f39f5771ec10
Octavian sima <octavian.sima98@gmail.com>
2020-10-08 00:30:21 +0000

    rename, copy arrays in enclave, removed user_check

--- server/enclave/ecalls.cpp
+++ server/enclave/ecalls.cpp
@@ -16,6 +16,25 @@
 
 using namespace std;
 
+#define check_host_buffer(ptr, size) {                    \
+if (!oe_is_outside_enclave((ptr), size)) {                \
+    fprintf(stderr,                                       \
+            "%s:%d: Buffer bounds check failed\n",        \
+            __FILE__, __LINE__);                          \
+    exit(1);                                              \
+}                                                         \
+}
+
+// Helper function used to copy double pointers from untrusted memory to enclave memory
+void copy_arr_to_enclave(unsigned char* dst[], size_t num, unsigned char* src[], size_t lengths[]) {
+  for (int i = 0; i < num; i++) {
+    size_t nlen = lengths[i];
+    check_host_buffer(src[i], nlen);
+    dst[i] = (unsigned char*) strndup((const char*) src[i], nlen);
+    dst[i][nlen] = '\0';
+  }
+}
+
 // This is the function that the host calls. It performs
 // the aggregation and encrypts the new model to pass back
 void enclave_modelaggregator(unsigned char*** encrypted_accumulator,
@@ -25,8 +44,17 @@ void enclave_modelaggregator(unsigned char*** encrypted_accumulator,
             size_t old_params_length, 
             unsigned char*** encrypted_new_params_ptr)
 {
+    size_t encryption_metadata_length = 3;
+
+    unsigned char* encrypted_old_params_cpy[encryption_metadata_length];
+    size_t lengths[] = {old_params_length, CIPHER_IV_SIZE, CIPHER_TAG_SIZE};
+    copy_arr_to_enclave(encrypted_old_params_cpy, 
+            encryption_metadata_length, 
+            encrypted_old_params, 
+            lengths);
+
     unsigned char serialized_old_params[old_params_length];
-    decrypt_bytes(*encrypted_old_params, 
+    decrypt_bytes(*encrypted_old_params_cpy, 
             *(encrypted_old_params + 1), 
             *(encrypted_old_params + 2), 
             old_params_length,
@@ -91,12 +119,12 @@ void enclave_modelaggregator(unsigned char*** encrypted_accumulator,
 
     string serialized_new_params = serialize(params);
 
-    unsigned char** encrypted_new_params = new unsigned char*[3 * sizeof(char*)];
+    unsigned char** encrypted_new_params = new unsigned char*[encryption_metadata_length * sizeof(char*)];
     encrypt_bytes((unsigned char*) serialized_new_params.c_str(), serialized_new_params.length(), encrypted_new_params);
 
     // Need to copy over the encrypted model, IV, and tag over to untrusted memory
-    unsigned char** usr_addr_params = (unsigned char**) oe_host_malloc(3 * sizeof(unsigned char *));
-    for (int i = 0; i < 3; i++) {
+    unsigned char** usr_addr_params = (unsigned char**) oe_host_malloc(encryption_metadata_length * sizeof(unsigned char *));
+    for (int i = 0; i < encryption_metadata_length; i++) {
         size_t item_length = strlen((const char *) encrypted_new_params[i]);
         unsigned char* item = (unsigned char*) oe_host_malloc(item_length * sizeof(unsigned char));
         memcpy((void*) item[i], encrypted_new_params[i], item_length * sizeof(unsigned char));
