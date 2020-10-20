8666789c85b9a9c544aa4dcdb2327069ae390149
Ubuntu <mc2@opaque-u4.z4gciyip1rvennk53qqlbasptg.bx.internal.cloudapp.net>
2020-10-20 00:40:28 +0000

    DEBUGGING

---
+++ server/server/enclave.h
@@ -0,0 +1,42 @@
+#include "modelaggregator_u.h"
+
+class Enclave {
+    private:
+        int num_clients;
+        oe_enclave_t* enclave_ref;
+        oe_result_t enclave_ret;
+
+    public:
+
+        Enclave(char* path, uint32_t flags)
+        {
+            enclave_ret = oe_create_modelaggregator_enclave(
+                path, OE_ENCLAVE_TYPE_AUTO, flags, NULL, 0, &this->enclave_ref);
+        }
+
+        oe_enclave_t* getEnclave() 
+        {
+            return this->enclave_ref;
+        }
+
+        oe_enclave_t** getEnclaveRef() 
+        {
+            return &(this->enclave_ref);
+        }
+
+        oe_result_t getEnclaveRet() 
+        {
+            return this->enclave_ret;
+        }
+
+
+        int set_num_clients(int num) 
+        {
+          num_clients = num;
+        }
+
+        int get_num_clients() 
+        {
+          return num_clients;
+        }
+};;
---
+++ server/server/enclave/CMakeFiles/simulate.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simulate"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/simulate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
