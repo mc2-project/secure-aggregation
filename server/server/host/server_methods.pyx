7603ca472694a606e58f2a4c0eed0d7177cf4572
Ubuntu <mc2@opaque-u4.z4gciyip1rvennk53qqlbasptg.bx.internal.cloudapp.net>
2020-10-20 02:10:30 +0000

    removing redundant directory

--- server/server/host/server_methods.pyx
+++
@@ -1,8 +0,0 @@
-# cython: language_level=3
-
-cdef extern from "host.h":
-    void host_modelaggregator(unsigned char*** encrypted_accumulator, 
-            size_t* accumulator_lengths,
-            size_t accumulator_length, 
-            unsigned char** encrypted_old_params,
-            size_t old_params_length)
