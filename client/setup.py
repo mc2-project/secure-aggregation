41c48817b3e20b09be3de76e9515868f24c41af1
Ubuntu <davidyi624@accvm.eqistvnhmize5ix5stdktur3lh.bx.internal.cloudapp.net>
2020-10-21 01:01:14 +0000

    adding installation option (non-cython)

--- client/setup.py
+++ client/setup.py
@@ -3,14 +3,36 @@ from setuptools.command.build_ext import build_ext as build_ext_orig
 
 from Cython.Build import cythonize
 
-setup(
-    ext_modules=cythonize(Extension(
-                          name="client_methods",
-                          sources=["client_methods.pyx", "../common/encryption/encrypt.cpp", "../common/encryption/serialization.cpp"],
-                          language="c++",
-                          libraries=["mbedtls", "mbedcrypto"],
-                          include_dirs=['/usr/include/mbedtls', '/usr/include/mbedcrypto']
-        ),
+# 
+try: 
+    from Cython.Distutils import build_ext
+except ImportError:
+    use_cython = False
+else:
+    use_cython = True
+
+if use_cython:
+    setup(
+        name = "client_methods",
+        ext_modules=cythonize(Extension(
+                            name="client_methods",
+                            sources=["client_methods.pyx", "../common/encryption/encrypt.cpp", "../common/encryption/serialization.cpp"],
+                            language="c++",
+                            libraries=["mbedtls", "mbedcrypto"],
+                            include_dirs=['/usr/include/mbedtls', '/usr/include/mbedcrypto']
+            ),
+        )
     )
-)
+else:
+    setup(
+        name = "client_methods",
+        ext_modules=cythonize(Extension(
+                            name="client_methods",
+                            sources=["client_methods.cpp", "../common/encryption/encrypt.cpp", "../common/encryption/serialization.cpp"],
+                            language="c++",
+                            libraries=["mbedtls", "mbedcrypto"],
+                            include_dirs=['/usr/include/mbedtls', '/usr/include/mbedcrypto']
+            ),
+        )
+    )
 
