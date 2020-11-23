79b39ad3437bc1bd78b49fc08bd5a42c5af63c6c
Octavian Sima <octavian.sima98@gmail.com>
2020-11-23 13:32:08 -0800

    resolved personal pathing issues

--- server/host/host.cpp
+++ server/host/host.cpp
@@ -14,8 +14,7 @@
 
 using namespace std;
 
-char* path = "/workspace/kvah/server/build/enclave/enclave.signed";
-//char* path = "/home/ubuntu/test/kvah/server/build/enclave/enclave.signed";
+char* path = "/root/code/secure-aggregation/server/build/enclave/enclave.signed"; 
 uint32_t flags = OE_ENCLAVE_FLAG_DEBUG | OE_ENCLAVE_FLAG_SIMULATE;
 
 // Helper function used to copy double pointers from untrusted memory to enclave memory
