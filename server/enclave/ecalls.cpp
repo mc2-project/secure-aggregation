8ea0a471f54e2ae8add75fdefa6184c112485acc
Octavian Sima <octavian.sima98@gmail.com>
2020-12-12 12:21:31 -0800

    avx implemented over weights loop

--- server/enclave/ecalls.cpp
+++ server/enclave/ecalls.cpp
@@ -142,14 +142,22 @@ void enclave_modelaggregator(int tid) {
             double n_iter = acc_params["_contribution"][0];
             iters_sum += n_iter;
 
-            // Multiple the weights by local iterations.
             vector<double>& weights = acc_params[v_name];
             if (g_old_params[v_name].size() != weights.size()) {
                 std::cout << "Error! Unequal sizes" << std::endl;
             }
 
-            for (int i = 0; i < weights.size(); i++) {
-                g_old_params[v_name][i] += weights[i] * n_iter;
+            // Multiple the weights by local iterations and update g_old_params[v_name].
+            const double n_iter_arr[4] = {n_iter, n_iter, n_iter, n_iter};
+            __m256d n_iter_slice = _mm256_loadu_pd(n_iter_arr);
+            for (int i = 0; i < weights.size(); i += 4) {
+                __m256d weights_slice = _mm256_loadu_pd((const double*) weights.data() + i);
+                __m256d old_params_v_name_slice = _mm256_loadu_pd((const double*) g_old_params[v_name].data() + i);
+
+                __m256d updated_old_params_v_name_slice = _mm256_add_pd(old_params_v_name_slice,
+                        _mm256_mul_pd(weights_slice, n_iter_slice));
+
+                _mm256_storeu_pd(g_old_params[v_name].data() + i, updated_old_params_v_name_slice);
             }
         }
 
